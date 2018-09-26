4a96288da0af40b3958365248ba8c66c0356aac3
nmagana <43592966+nmagana@users.noreply.github.com>
2018-09-25 23:59:34 -0700

    Create string-basics.js

---
+++ JavaScript-Review/string-basics.js
@@ -0,0 +1,18 @@
+# Lesson 1: String Basics
+
+# You create variables in JavaScript by using let
+let color = 'blue'
+
+# Following line is used to output to the console
+console.log(color)
+
+# Now that thisVariable is declared, you can reassign without using let
+color = 'green'
+console.log(color)
+
+let food = 'eggs and ham'
+
+# Variables can be created by concatenating variables and string literals
+let myOrder = 'I want ' + color + ' ' + food + ' please!'
+console.log(myOrder)
+
