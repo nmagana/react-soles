dfb38f60fce011f6c2ec66bdb69828a190f338da
nmagana <43592966+nmagana@users.noreply.github.com>
2018-09-27 15:28:28 -0700

    Create number-basics.js

---
+++ JavaScript-Review/number-basics.js
@@ -0,0 +1,22 @@
+// Lesson 2: Getting familiar with number operations
+
+let number = 10
+
+// add numbers
+let addition = number + 10
+console.log(addition)
+
+// subtract numbers
+let subtraction = number - 10
+console.log(subtraction)
+
+// multiply numbers
+let multiplication = number * 10
+console.log(multiplication)
+
+let division = number / 10 
+console.log(division)
+
+// use parentheses to allow operations to happen before others
+let paranthesesExample = (number + 5) * 10
+console.log(paranthesesExample)
