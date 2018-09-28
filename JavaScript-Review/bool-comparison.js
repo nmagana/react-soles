6e8de024c34f3b5e0f162c6d879eebabb3fa744b
Nicolas Magana <nmagana@nicolass-mbp.attlocal.net>
2018-09-28 16:00:20 -0700

    Added .gitignore and added bool-comparison.js file

---
+++ JavaScript-Review/bool-comparison.js
@@ -0,0 +1,36 @@
+// Lesson 3: Bool and Comparison Operators
+/* 
+=== is True when type and value are equal. For example:
+3 === 3 evaluates to True, while 3 === '3' evaluates to False.
+*/
+
+/* 
+== is True when value is equal. Important note is that type does not have to be the same. For example:
+3 == 3 evaluates to True, and 3 == '3' also evaluates to True.
+*/
+
+console.log('Ucla' === 'Ucla') // True
+console.log('UCLA' === 'ucla') // False
+console.log(3 === 3) // True
+console.log(3 == '3') // True
+console.log(3 === '3') // False
+
+// !== Returns true if the operands are of the same type but not equal, or are of different type, false otherwise.
+// != Returns true if the operands are equal but not the same type, false otherwise.
+
+console.log('Ucla' !== 3) // True
+console.log('Ucla' !== 'Ucla') // False
+console.log(3 !== '3') // True
+console.log(3 != '3') // False
+
+/* the following comparison operators exist as well:
+> greater than
+>= greater than or equal to 
+< less than
+<= less than or equal to
+*/
+
+console.log(4 > 3) // True
+console.log(4 >= 4) // True
+console.log(4 < 4) // False
+console.log(3 <= 4) // True
