e47db2246e668c121062d364809d52e130b822ac
Nicolas Magana <nmagana2112@gmail.com>
2018-09-29 16:47:08 -0700

    Created Exercise 1

--- JavaScript-Review/functions/exercise-1.js
+++ JavaScript-Review/functions/exercise-1.js
@@ -1,1 +1,19 @@
-// Exercise 1: 
+// Exercise 1: Function Practice
+// In this exercise, you will be creating functions for different scenarios.
+// Don't worry about invalid input being passed in.
+
+
+// Function 1: Feet to Kilometer Converter
+// Create a function that takes in feet, and converts it to kilometers. Return the result.
+
+// Function 2: Tip Calculator
+// Create a function that takes in the amount that the meal cost, and a tip given as a decimal.
+// Have the default value of the tip be 0.15. Return the amount that should be tipped.
+
+// Function 3: Should I wear a jacket?
+// Create a function that takes in the temperature (Assume Farenheit) and a bool if it is raining.
+// Set the default for the bool to be false.
+// Print to the console "You should wear a jacket" for the following scenarios:
+// the temperature is less than 40.
+// the temperature is between 40-80 inclusive and it is raining.
+// All other scenarios print "You do not need to wear a jacket"
