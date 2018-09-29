6c575adda335fd51cc6a59c73f5bc30cdf7bfc97
Nicolas Magana <nmagana2112@gmail.com>
2018-09-28 17:52:19 -0700

    Re-added .vscode to .gitignore and finished scope.js

---
+++ JavaScript-Review/scope.js
@@ -0,0 +1,43 @@
+// Lesson 5: Variable Scope
+
+// JavaScript uses Lexical (Static) Scoping. This means that the block, or scope, 
+// in which variables are created plays a big part if it can get accessed
+
+/*
+There are two types of scope:
+
+Global Scope - Variables defined outside any code block and can be accessed
+anywhere within the program.
+
+Local Scope - Variables that are defined inside of a code block.
+*/
+
+/* 
+Shadowing - When a variable in an inner scope with the same name as a variable
+in an outer scope assigns a different value.
+*/
+
+let var1 = 4; // global scope
+if (true) {
+    let var1 = 3 // local scope, example of shadowing
+    console.log(var1) // Prints 3
+}
+console.log(var1) // Prints 4
+
+/* 
+Leaked Globals - When a variable is used but has not been declared by using let,
+the compiler creates it as a global variable. This can cause bugs. Always
+make sure to use the let keyword when making a variable.
+*/
+
+// Example of Leaked Global
+
+// Don't worry about the syntax for a function yet, it will be covered in the next lesson!
+let myFunc = function() {
+    bruins = 5
+    console.log(bruins)
+}
+
+myFunc()
+console.log(bruins) // this will still work because now the compiler treats it as a global!
+
