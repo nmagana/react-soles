c21b1f6bbf5fdce7f343a00fe1571f97e017a726
nmagana <nmagana2112@gmail.com>
2018-10-03 13:31:35 -0700

    Updated comments in math-basics

--- JavaScript-Review/1-basics/2-number-basics.js
+++ JavaScript-Review/1-basics/2-number-basics.js
@@ -1,21 +1,22 @@
-// Lesson 2: Getting familiar with number operations
-// In this lesson you will learn the basic operations for integers as well as
-// using some methods used by integers and the Math library.
+/* Lesson 2: Getting familiar with number operations
+In this lesson you will learn the basic operations for integers as well as using methods used by 
+integers and the Math library. */
 
 let number = 10;
 
-// add numbers
+// add numbers with +
 let addition = number + 10;
 console.log(addition);
 
-// subtract numbers
+// subtract numbers with -
 let subtraction = number - 10;
 console.log(subtraction);
 
-// multiply numbers
+// multiply numbers with *
 let multiplication = number * 10;
 console.log(multiplication);
 
+// divide numbers with /
 let division = number / 10;
 console.log(division);
 
@@ -23,22 +24,22 @@ console.log(division);
 let paranthesesExample = (number + 5) * 10;
 console.log(paranthesesExample);
 
-// Below are some methods used for ints as well as using the Math library
+// Below are some methods used for integer and examples usign the Math library.
 let num = 69.6969
 
-// Set number of digits after decimal point
+// Set number of digits after decimal point.
 console.log(num.toFixed(2))
 
-// Round numbers to the nearest whole integer
+// Round numbers to the nearest whole integer.
 console.log(Math.round(num))
 
-// Rounds number down 
+// Rounds number down.
 console.log(Math.floor(num))
 
-// Rounds number up
+// Rounds number up.
 console.log(Math.ceil(num))
 
-// Use this to create a range of random numbers
+// Use this to create a range of random numbers.
 let min = 420
 let max = 1919
 let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
