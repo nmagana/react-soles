3e090cddfd4e929c269e654b84b6148aa3a0c8b8
nmagana <nmagana2112@gmail.com>
2018-10-03 13:41:17 -0700

    Updated extra-types

--- JavaScript-Review/1-basics/6-extra-types.js
+++ JavaScript-Review/1-basics/6-extra-types.js
@@ -1,8 +1,8 @@
-// Lesson 6: Extra Types
-// In this lesson you will explore the undefined, null, and const.
+/* Lesson 6: Extra Types
+In this lesson you will explore the undefined, null, and const. */
 
-// Variables will be assigned the value of undefined if they are not assigned a value.
-// This value is given by the compiler.
+/* Variables will be assigned the value of undefined if they are not assigned a value.
+This value is given by the compiler. */
 
 let myVar
 console.log(myVar) // undefined will be printed
@@ -11,8 +11,8 @@ if (myVar === undefined) {
     console.log('I am undefined!')
 }
 
-// Variables can be explicitly be assigned the value of null.
-// Null can be useful if you want a value to be programically empty.
+/* Variables can be explicitly be assigned the value of null.
+Null can be useful if you want a value to be programically empty. */
 
 myVar = null
 
@@ -20,9 +20,9 @@ if (myVar === null) {
     console.log('I am null!')
 }
 
-// The keyword const is used when you want to assign a variable that you
-// don't want to be changed for the rest of the program. If the variable
-// is modified, an error will be generated.
+/* The keyword const is used when you want to assign a variable that you
+don't want to be changed for the rest of the program. If the variable
+is modified, an error will be generated. */
 
 const num = 5
 // num = 3 <= This will break!
