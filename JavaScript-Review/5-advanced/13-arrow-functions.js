0691308b5194b363d54c4a86d436f0ea456446bc
nmagana <nmagana2112@gmail.com>
2018-10-03 14:40:12 -0700

    Update 13-arrow-functions.js

--- JavaScript-Review/5-advanced/13-arrow-functions.js
+++ JavaScript-Review/5-advanced/13-arrow-functions.js
@@ -1,6 +1,6 @@
-// Lesson 13: Arrow Functions
-// In this lesson, you will learn how to write arrow functions as well
-// as learn about the Conditional (Ternary Operator)
+/* Lesson 13: Arrow Functions
+In this lesson, you will learn how to write arrow functions as well
+as learn about the Conditional (Ternary Operator) */
 
 // regular function
 let multiplication = function(arg1, arg2) {
@@ -16,8 +16,8 @@ multiplication = (arg1, arg2) => {
 
 console.log(multiplication(1,2))
 
-// You can even write it in simpler syntax.
-// No return statement is required, as it acts like an implied return statement.
+/* You can even write it in simpler syntax.
+No return statement is required, as it acts like an implied return statement. */
 multiplication = (arg1, arg2) => arg1 * arg2;
 console.log(multiplication(1,2));
 
@@ -33,15 +33,14 @@ let user = {
 }
 
 user.getName() // undefined is printed
-// this occurs because the this keyword is trying
-// to find name within its current scope, but there is no name variable
-// in its scope
+/* This occurs because the this keyword is trying to find name within its current 
+scope, but there is no name variable in its scope. */
 
-// use arrows functions instead, which will actually grab
-// the this value from its parent, therefore being able
-// to get name in our example. However, because of this, don't use
-// arrow functions as methods in the object, since the this object
-// will be initialize to undefined
+/* Use arrows functions instead, which will actually grab
+the this value from its parent, therefore being able
+to get name in our example. However, because of this, don't use
+arrow functions as methods in the object, since the this object
+will be initialized to undefined. */
 
 user = {
     name: 'Nico',
@@ -55,8 +54,8 @@ user = {
 
 user.getName() // will actually print Nico
 
-// Ternary Operator Syntax
-// condition ? Value if true: Value if false
+/* Ternary Operator Syntax
+condition ? Value if true: Value if false */
 let firstNum = 7;
 let secondNum = 8;
 let isMultiplication = true;
