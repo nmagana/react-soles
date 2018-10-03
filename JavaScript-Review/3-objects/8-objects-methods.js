1646a137fde948a056886644f133865cd56c1ed9
nmagana <nmagana2112@gmail.com>
2018-10-03 13:55:22 -0700

    Updated comments in objects-methods

--- JavaScript-Review/3-objects/8-objects-methods.js
+++ JavaScript-Review/3-objects/8-objects-methods.js
@@ -1,9 +1,9 @@
-// Lesson 8: Objects Methods
-// In this lesson, you will learn how to include functions
-// in objects, and learn how to use the this pointer.
+/* Lesson 8: Objects Methods
+In this lesson, you will learn how to include functions in objects, and learn how to use the this keyword. */
 
-// Syntax to create object with properties and methods
-// The this keyword is used in an object to reference properties that the object has
+/* Syntax to create object with properties and methods
+The this keyword is used in an object to reference properties that the object has. */
+
 let classroom = {
     capacity: 100,
     enrolled: 50,
