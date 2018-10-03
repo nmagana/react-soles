338549a11ec30b0a33e23798fed91965fb18e649
nmagana <nmagana2112@gmail.com>
2018-10-03 13:29:22 -0700

    Updated comments in string-basics

--- JavaScript-Review/1-basics/1-string-basics.js
+++ JavaScript-Review/1-basics/1-string-basics.js
@@ -1,39 +1,40 @@
-// Lesson 1: String Basics
-// In this lesson you will learn about string operations as well as
-// explore string methods and properties.
 
-// You create variables in JavaScript by using let
+/* Lesson 1: String Basics
+In this lesson you will learn about string operations and explore string methods and properties. */
+
+// You can create variables in JavaScript by using let.
 let color = 'blue';
 
-// Following line is used to output to the console
+// The following line is used to output to the console.
 console.log(color);
 
-// Now that thisVariable is declared, you can reassign without using let
+// Now that color is declared, you can reassign without using let.
 color = 'green';
 console.log(color);
 
 let food = 'eggs and ham';
 
-// Variables can be created by concatenating variables and string literals
+// Variables can be created by concatenating variables and string literals.
 let myOrder = 'I want ' + color + ' ' + food + ' please!';
 console.log(myOrder);
 
-// To include variables directly in string, you can use Template Strings. This avoids using +
-// Wrap the string with `` and use ${variable_name_here} to put in a variable directly in the string.
+/* To include variables directly in string, you can use Template Strings. This avoids using +.
+Wrap the string with `` and use ${variable_name_here} to put in a variable directly in the string. */
 
 myOrder = `I want ${color} ${food} please!`;
 console.log(myOrder);
 
-// Below are some useful properties and methods that string variables have
+// Below are some useful properties and methods that strings have.
 
 let school = 'Ucla'
-// Length property, gets how many characters are in string
+
+// Length property, returns how many characters are in a string.
 console.log(school.length)
 
-// Method that converts string to Upper Case
+// Method that converts string to Upper Case. 
 console.log(school.toUpperCase())
 
-// Method that converts string to Lower Case
+// Method that converts string to Lower Case. 
 console.log(school.toLowerCase())
 
 let password = 'geneUCLAblock'
