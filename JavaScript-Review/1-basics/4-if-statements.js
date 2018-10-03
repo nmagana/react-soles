aff6801ed74575f6f3ba9e829f8a4b987c738684
nmagana <nmagana2112@gmail.com>
2018-10-03 13:38:24 -0700

    Update comments in if-statements

--- JavaScript-Review/1-basics/4-if-statements.js
+++ JavaScript-Review/1-basics/4-if-statements.js
@@ -1,16 +1,15 @@
-// Lesson 4: If Statements
-// In this lesson you will learn how to control the flow of statements by using
-// if, else if, and else, while also learning about logical operators.
+/* Lesson 4: If Statements
+ In this lesson you will learn how to control the flow of statements by using
+ if, else if, and else, while also learning about logical operators. */
 
 let myVar = 10;
 
-// syntax for if statements
+// Syntax for if statements.
 if (myVar > 5){
     console.log('I am greater than 5!');
 }
 
-// you can also add else if statements to create more control
-// add an else statement to catch all other options
+//You can also add else if statements to create more control. Add an else statement to catch all other options 
 if (myVar < 4){
     console.log('I am less than 4!');
 } else if (myVar > 12){
@@ -19,10 +18,10 @@ if (myVar < 4){
     console.log('I am everything else');
 }
 
-// You can also use operators inside expressions to create different types of expressions
-// ! (Logical Not) - turns True to False, False to True
-// && (Logical And) - Evaluates to True if the left side and right side are both True, False otherwise
-// || (Logical Or) - Evaluates to True if either the left side or right side are True, False otherwise
+/* You can also use operators inside expressions to create different types of expressions.
+! (Logical Not) - turns True to False, False to True
+&& (Logical And) - Evaluates to True if the left side and right side are both True, False otherwise
+|| (Logical Or) - Evaluates to True if either the left side or right side are True, False otherwise */
 
 if (!(myVar < 4)){
     console.log('I am not less than 4!');
@@ -34,7 +33,7 @@ if (myVar < 4 && myVar > 7) {
     console.log('Second statement was hit!');
 }
 
-// previous statement works if the && is replaced with ||
+// Previous statement works if the && is replaced with ||.
 
 if (myVar < 4 || myVar > 7) {
     console.log('First statement was hit!');
