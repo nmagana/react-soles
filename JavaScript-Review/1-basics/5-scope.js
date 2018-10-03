7e430cdc150c6f369e93ce9f28abbb4d49696efc
nmagana <nmagana2112@gmail.com>
2018-10-03 13:39:45 -0700

    Updated comments in scope

--- JavaScript-Review/1-basics/5-scope.js
+++ JavaScript-Review/1-basics/5-scope.js
@@ -1,23 +1,19 @@
-// Lesson 5: Variable Scope
-// In this lesson , you will learn about different types of scopes
-// in JavaScript.
+/* Lesson 5: Variable Scope
+In this lesson , you will learn about different types of scopes
+in JavaScript. */
 
-// JavaScript uses Lexical (Static) Scoping. This means that the block, or scope, 
-// in which variables are created plays a big part if it can get accessed
+/* JavaScript uses Lexical (Static) Scoping. This means that the block, or scope, 
+in which variables are created plays a big part if it can get accessed */
 
-/*
-There are two types of scope:
+/* There are two types of scope:
 
 Global Scope - Variables defined outside any code block and can be accessed
 anywhere within the program.
 
-Local Scope - Variables that are defined inside of a code block.
-*/
+Local Scope - Variables that are defined inside of a code block. */
 
-/* 
-Shadowing - When a variable in an inner scope with the same name as a variable
-in an outer scope assigns a different value.
-*/
+/* Shadowing - When a variable in an inner scope with the same name as a variable
+in an outer scope assigns a different value. */
 
 let var1 = 4; // global scope
 if (true) {
@@ -26,20 +22,18 @@ if (true) {
 }
 console.log(var1); // Prints 4
 
-/* 
-Leaked Globals - When a variable is used but has not been declared by using let,
+/* Leaked Globals - When a variable is used but has not been declared by using let,
 the compiler creates it as a global variable. This can cause bugs. Always
-make sure to use the let keyword when making a variable.
-*/
+make sure to use the let keyword when making a variable. */
 
 // Example of Leaked Global
-
 // Don't worry about the syntax for a function yet, it will be covered in the next lesson!
+
 let myFunc = function() {
     bruins = 5;
     console.log(bruins);
 }
 
 myFunc();
-console.log(bruins); // this will still work because now the compiler treats it as a global!
+console.log(bruins); // This will still work because now the compiler treats it as a global!
 
