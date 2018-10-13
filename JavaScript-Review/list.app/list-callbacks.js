3166b8b8bbbb2957784d0ffaaec0b28d1a027617
nmagana <nmagana2112@gmail.com>
2018-10-12 18:17:49 -0700

    Update list-callbacks.js

--- JavaScript-Review/list.app/list-callbacks.js
+++ JavaScript-Review/list.app/list-callbacks.js
@@ -1,3 +1,11 @@
+/*
+    These are functions that are used as callbacks for other functions!
+*/
+
+/*
+    Sort function that sorts based on alphabetical order,
+    regardless of being capitalized or not.
+*/
 let sortFunction = function(a, b) {
     a_lower = a.toLowerCase();
     b_lower = b.toLowerCase();
@@ -6,12 +14,20 @@ let sortFunction = function(a, b) {
     else {return 1;}
 };
 
+/*
+    List call back that creates a paragraph tag and appends to the
+    element with the #list id.
+*/
 let createListCallBack = function(list_item) {
     const listElement = document.createElement('p');
     listElement.textContent = list_item;
     document.querySelector('#list').appendChild(listElement);
 };
 
+/*
+   Returns a callback function that passes in a word and checks
+   if the list item starts with that word.
+*/
 let listFilter = function(word) {
     return function(list_item) {
         return list_item.toLowerCase().startsWith(word.toLowerCase());
