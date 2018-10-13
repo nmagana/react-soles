f0eeaf28ba9b0469c8fd8a2befd6d0ac88dcde78
nmagana <nmagana2112@gmail.com>
2018-10-12 18:22:01 -0700

    Update list-functions.js

--- JavaScript-Review/list.app/list-functions.js
+++ JavaScript-Review/list.app/list-functions.js
@@ -1,8 +1,15 @@
+/*
+    Creates a list based on the filter that is currently inputted.
+    Creates a new paragragh tag for each list item with the forEach function.
+*/
 let createList = function(filter) {
     filteredList = appEnviron.list.filter(listFilter(filter));
     filteredList.forEach(createListCallBack);
 };
 
+/*
+    Clears all the paragraph tags by setting the innerHtml to an empty string.
+*/
 let clearList = function() {
     document.querySelector('#list').innerHTML = "";
 };
