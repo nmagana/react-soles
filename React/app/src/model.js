a454e16c68896d1042762954b0aa3224dbfc0e29
Nicolas Magana <nmagana2112@gmail.com>
2018-10-31 21:00:28 -0700

    Added model file

---
+++ React/app/src/model.js
@@ -0,0 +1,80 @@
+// This code won't be rendered, but will just be used as a reference to
+// where we are in the course!
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class DysfunctionalApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: this.props.options,
+            selectedOption: undefined
+        };
+    }
+    render() {
+        const subtitle = 'Let the computer choose!';
+        return (
+            <div>
+                <Header subtitle={subtitle}/>
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
+
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.title}</p>
+                {this.props.subtitle && <p>{this.props.subtitle}</p>}
+            </div>
+        );
+    }
+}
+
+Header.defaultProps = {
+    title: 'dysFUNctional'
+};
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What Should I do?</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Please put in an option to get started!</p>
+                <button>Remove All</button>
+                <p>Render Options Here</p>
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input name="option_input" autoComplete="off"/>
+                    <button>Add Option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<DysfunctionalApp />, document.getElementById('app'));
